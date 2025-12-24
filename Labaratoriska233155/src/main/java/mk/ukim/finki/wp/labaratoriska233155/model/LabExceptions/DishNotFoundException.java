bc2560ed6ef248c0e3dc84a5a25ae60f5fcd6a57
mindosh <stefmindosh@gmail.com>
2025-12-24 22:40:11 +0100

    Delete Labaratoriska233155 directory

--- Labaratoriska233155/src/main/java/mk/ukim/finki/wp/labaratoriska233155/model/LabExceptions/DishNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package mk.ukim.finki.wp.labaratoriska233155.model.LabExceptions;
-
-public class DishNotFoundException extends RuntimeException{
-    public DishNotFoundException() {
-        super("Dish was not found!");
-    }
-}
--- Labaratoriska233155/src/main/java/mk/ukim/finki/wp/labaratoriska233155/model/LabExceptions/NotSelectedChefException.java
+++
@@ -1,7 +0,0 @@
-package mk.ukim.finki.wp.labaratoriska233155.model.LabExceptions;
-
-public class NotSelectedChefException extends RuntimeException{
-    public NotSelectedChefException(){
-        super("Please select a chef!");
-    }
-}
