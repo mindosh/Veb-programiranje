9e9d780e585b73fba84d946cc9a43567a4702f6a
mindosh <stefmindosh@gmail.com>
2025-12-24 22:40:18 +0100

    Delete LabaratoriskaVezba1_233155 directory

--- LabaratoriskaVezba1_233155/src/main/java/mk/ukim/finki/wp/lab/model/LabExceptions/DishNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package mk.ukim.finki.wp.lab.model.LabExceptions;
-
-public class DishNotFoundException extends RuntimeException{
-    public DishNotFoundException() {
-        super("Dish was not found!");
-    }
-}
--- LabaratoriskaVezba1_233155/src/main/java/mk/ukim/finki/wp/lab/model/LabExceptions/NotSelectedChefException.java
+++
@@ -1,7 +0,0 @@
-package mk.ukim.finki.wp.lab.model.LabExceptions;
-
-public class NotSelectedChefException extends RuntimeException{
-    public NotSelectedChefException(){
-        super("Please select a chef!");
-    }
-}
